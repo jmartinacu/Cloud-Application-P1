87d4166fcc25d20e67a309ce7921ef6c8d7b9895
carloosbaquero <carlos.baqueville@gmail.com>
2022-10-13 15:26:09 +0200

    restart

--- proyecto1_B/files-js/src/models/file.js
+++
@@ -1,9 +0,0 @@
-const mariadb = require("mariadb");
-
-var Schema = mariadb.Schema({
-    date: {type:Date, default: Date.now},
-    description: String,
-    image: String
-});
-
-module.exports = mariadb.model('File', FileSchema);
